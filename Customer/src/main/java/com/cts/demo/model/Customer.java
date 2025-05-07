69bfaa6884f7fc4ca37ba3e2db573425d1ad1d73
Gowtham <gowthamjagadeesan322@gmail.com>
2025-05-07 22:04:04 +0530

    customer module completed successfully

--- Customer/src/main/java/com/cts/demo/model/Customer.java
+++ Customer/src/main/java/com/cts/demo/model/Customer.java
@@ -9,6 +9,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,14 +24,20 @@ import lombok.NoArgsConstructor;
 @Table(name = "customer")
 public class Customer {
 	@Id
+	@Positive(message = "The value should not be negative")
 	private long customerId;
+	@NotEmpty(message = "Customer Name should not be null or blank")
 	private String customerName;
+	@Email(message = "Enter a valid email Id")
 	private String customerEmail;
+	@Size(max = 12, min = 10, message = "Enter a valid phone number")
 	private String customerPhone;
+	@NotEmpty(message = "Address should not be empty")
 	private String customerAddress;
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	@JoinColumn(name = "customer_id", referencedColumnName = "customerId")
 	private List<Policy1> policies;
+
 	public Customer(String customerName, String customerEmail, String customerPhone, String customerAddress,
 			List<Policy1> policies) {
 		super();
@@ -37,8 +47,5 @@ public class Customer {
 		this.customerAddress = customerAddress;
 		this.policies = policies;
 	}
-
-	
-	
 
 }
--- Customer/src/test/java/com/cts/demo/CustomerApplicationTests.java
+++ Customer/src/test/java/com/cts/demo/CustomerApplicationTests.java
@@ -1,8 +1,8 @@
 package com.cts.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mockitoSession;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.cts.demo.exception.CustomerNotFoundException;
 import com.cts.demo.model.Customer;
+import com.cts.demo.model.Policy1;
 import com.cts.demo.repository.CustomerRepository;
 import com.cts.demo.service.CustomerServiceImpl;
 
@@ -27,63 +28,100 @@ class CustomerApplicationTests {
 	@InjectMocks
 	CustomerServiceImpl service;
 
-//	@Test
-//	void saveTest() {
-//		Customer customer = new Customer(2113017, "Bharath", "bharathrahmuthukumaran@gmail.com", "6382669477", "salem",
-//				67890);
-//		Mockito.when(repository.save(customer)).thenReturn(customer);
-//		String response = service.saveCustomer(customer);
-//		assertEquals("Customer saved succussfully", response);
-//	}
-//
-//	@Test
-//	void updateTest() {
-//		Customer customer = new Customer(2113017, "Bharath", "bharathrahmuthukumaran@gmail.com", "6382669477", "salem",
-//				67890);
-//		Mockito.when(repository.save(customer)).thenReturn(customer);
-//		Customer cust = service.updateCustomer(customer);
-//		assertEquals(customer, cust);
-//	}
-//
-//	@Test
-//	void deleteTest() {
-//		Customer customer = new Customer(2113017, "Bharath", "bharathrahmuthukumaran@gmail.com", "6382669477", "salem",
-//				67890);
-//		long userId = customer.getCustomerId();
-//		Mockito.doNothing().when(repository).deleteById(userId);
-//		String response = service.deleteCustomer(userId);
-//		assertEquals("Customer deleted successfully", response);
-//
-//	}
-//
-//	@Test
-//	void searchCustomerByIdTest() throws CustomerNotFoundException {
-//		Customer customer = new Customer("Bharath", "bharathrahmuthukumaran@gmail.com", "6382669477", "salem", 67890);
-//		long customerId = 21113016;
-//		customer.setCustomerId(customerId);
-//		Mockito.when(repository.findById(customerId)).thenReturn(Optional.of(customer));
-//		Customer foundCustomerById = service.searchCustomerById(customerId);
-//		assertEquals(customer, foundCustomerById);
-//	}
-//
-//	@Test
-//	void searchCustomerByNameTest() {
-//		String customerName = "Bharath";
-//		Customer customer = new Customer(2113017, "Bharath", "bharathrahmuthukumaran@gmail.com", "6382669477", "salem",
-//				67890);
-//		Mockito.when(repository.findByCustomerName(customerName)).thenReturn(customer);
-//		Customer foundCustomerByName = service.searchCustomerByName(customerName);
-//		assertEquals(customer, foundCustomerByName);
-//	}
-//
-//	@Test
-//	void getAllCustomerTest() {
-//		List<Customer> customers = Arrays.asList(
-//				new Customer(2113017, "Bharath", "bharathrahmuthukumaran@gmail.com", "6382669477", "salem", 67890),
-//				new Customer(2113016, "Bharathi", "bharathiannadurai@gmail.com", "9443126900", "erode", 67891));
-//		Mockito.when(repository.findAll()).thenReturn(customers);
-//		List<Customer> custs = service.getAllCustomer();
-//		assertEquals(customers, custs);
-//	}
+	@Test
+	void saveTest() {
+		Policy1 p1 = new Policy1();
+		p1.setPolicyType("Health");
+		p1.setPolicyId(201);
+		List<Policy1> policies = new ArrayList<>();
+		policies.add(p1);
+		Customer customer = new Customer(2113017, "Bharath", "bharathrahmuthukumaran@gmail.com", "6382669477", "salem",
+				policies);
+		Mockito.when(repository.save(customer)).thenReturn(customer);
+		String response = service.saveCustomer(customer);
+		assertEquals("Customer saved succussfully", response);
+	}
+
+	@Test
+	void updateTest() {
+		Policy1 p1 = new Policy1();
+		p1.setPolicyType("Health");
+		p1.setPolicyId(201);
+		List<Policy1> policies = new ArrayList<>();
+		policies.add(p1);
+		Customer customer = new Customer(2113017, "Bharath", "bharathrahmuthukumaran@gmail.com", "6382669477", "salem",
+				policies);
+		Mockito.when(repository.save(customer)).thenReturn(customer);
+		Customer cust = service.updateCustomer(customer);
+		assertEquals(customer, cust);
+	}
+
+	@Test
+	void deleteTest() {
+		Policy1 p1 = new Policy1();
+		p1.setPolicyType("Health");
+		p1.setPolicyId(201);
+		List<Policy1> policies = new ArrayList<>();
+		policies.add(p1);
+		Customer customer = new Customer(2113017, "Bharath", "bharathrahmuthukumaran@gmail.com", "6382669477", "salem",
+				policies);
+		long userId = customer.getCustomerId();
+		Mockito.doNothing().when(repository).deleteById(userId);
+		String response = service.deleteCustomer(userId);
+		assertEquals("Customer deleted successfully", response);
+
+	}
+
+	@Test
+	void searchCustomerByIdTest() throws CustomerNotFoundException {
+		Policy1 p1 = new Policy1();
+		p1.setPolicyType("Health");
+		p1.setPolicyId(201);
+		List<Policy1> policies = new ArrayList<>();
+		policies.add(p1);
+		Customer customer = new Customer("Bharath", "bharathrahmuthukumaran@gmail.com", "6382669477", "salem",
+				policies);
+		long customerId = 21113016;
+		customer.setCustomerId(customerId);
+		Mockito.when(repository.findById(customerId)).thenReturn(Optional.of(customer));
+		Customer foundCustomerById = service.searchCustomerById(customerId);
+		assertEquals(customer, foundCustomerById);
+	}
+
+	@Test
+	void searchCustomerByNameTest() {
+		String customerName = "Bharath";
+		Policy1 p1 = new Policy1();
+		p1.setPolicyType("Health");
+		p1.setPolicyId(201);
+		List<Policy1> policies = new ArrayList<>();
+		policies.add(p1);
+		Customer customer = new Customer(2113017, "Bharath", "bharathrahmuthukumaran@gmail.com", "6382669477", "salem",
+				policies);
+		Mockito.when(repository.findByCustomerName(customerName)).thenReturn(customer);
+		Customer foundCustomerByName = service.searchCustomerByName(customerName);
+		assertEquals(customer, foundCustomerByName);
+	}
+
+	@Test
+	void getAllCustomerTest() {
+		Policy1 p1 = new Policy1();
+		p1.setPolicyType("Health");
+		p1.setPolicyId(201);
+		List<Policy1> policies = new ArrayList<>();
+		policies.add(p1);
+
+		Policy1 p2 = new Policy1();
+		p2.setPolicyType("Gold");
+		p2.setPolicyId(202);
+		List<Policy1> policies2 = new ArrayList<>();
+		policies2.add(p2);
+		List<Customer> customers = Arrays.asList(
+				new Customer(2113017, "Bharath", "bharathrahmuthukumaran@gmail.com", "6382669477", "salem", policies),
+				new Customer(2113016, "Bharathi", "bharathiannadurai@gmail.com", "9443126900", "erode", policies2));
+		Mockito.when(repository.findAll()).thenReturn(customers);
+		List<Customer> custs = service.getAllCustomer();
+		assertEquals(customers, custs);
+	}
 
 }
