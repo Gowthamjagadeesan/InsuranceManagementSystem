fc09b83f1b4ea5124c2e6f0ef300a65c6305bef1
Gowtham <gowthamjagadeesan322@gmail.com>
2025-05-13 23:31:11 +0530

    changes in notify

--- Notification/src/test/java/com/cts/demo/NotificationApplicationTests.java
+++ Notification/src/test/java/com/cts/demo/NotificationApplicationTests.java
@@ -1,13 +1,31 @@
 package com.cts.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.cts.demo.entity.Notification;
+import com.cts.demo.repository.NotificationRepository;
+import com.cts.demo.service.NotificationServiceImpl;
 
 @SpringBootTest
 class NotificationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Mock
+	NotificationRepository notificationRepository;
 
+	@InjectMocks
+	NotificationServiceImpl notificationService;
+
+	@Test
+	void savePolicy() {
+		Notification notification = new Notification(123, 1234, 70000, "hospinal expences");
+		Mockito.when(notificationRepository.save(notification)).thenReturn(notification);
+		String response = notificationService.saveNotification("hospinal expences", 1234, 70000);
+		assertEquals("Notification saved Successfully", response);
+	}
 }
